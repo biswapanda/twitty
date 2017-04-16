1345165c1f5266af0fed1ec1d9326ef194a9498e
bis <biswa.panda@gmail.com>
2017-04-16 16:05:01 -0700

    Add LoginViewController

--- twitty/LoginViewController.swift
+++ twitty/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  twitty
 //
 //  Created by bis on 4/16/17.
@@ -8,18 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginTaped(_ sender: Any) {
+        print("login tapped")
     }
 
-
 }
-
